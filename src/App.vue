04d01fcdc7bd33c960cd752cf7917222b74309ab
Matt Miller <matt@megamattmiller.com>
2019-06-04 19:53:48 -0500

    feat: good-enough working site for now

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,11 @@
 </template>
 
 <style>
+#app {
+  padding: 0;
+  margin: 0;
+  font-family: 'Fira Sans', sans-serif;
+  width: 100%;
+  min-height: 100vh;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,47 @@
 <template>
   <div class="home">
-    <div id="termynal" data-termynal data-ty-type-delay="40" data-ty-line-delay="700">
-      <span data-ty="input">pub global activate pubversion</span>
-      <span data-ty>Resolving dependencies...</span>
-      <span data-ty>Activated pubversion 1.0.0</span>
-      <span data-ty="input">pubversion major</span>
-      <span data-ty>example_lib upgraded from 1.0.2 to 2.0.0</span>
+    <div class="header">
+      <h1>pubversion</h1>
+    </div>
+    <div class="terminal">
+      <div id="termynal" data-termynal data-ty-type-delay="40" data-ty-line-delay="700">
+        <span data-ty="input">pub global activate pubversion</span>
+        <span data-ty>Resolving dependencies...</span>
+        <span data-ty>Activated pubversion 1.0.0</span>
+        <span data-ty="input">pubversion major</span>
+        <span data-ty>example_lib upgraded from 1.0.2 to 2.0.0</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 export default {
   name: "home",
-  components: {}
+  components: {},
+  mounted() {
+    let termynalScript = document.createElement("script");
+    termynalScript.setAttribute(
+      "src",
+      "https://s3-us-west-2.amazonaws.com/s.cdpn.io/316871/termynal.min.js"
+    );
+    termynalScript.setAttribute("data-termynal-container", "#termynal");
+    document.head.appendChild(termynalScript);
+  }
 };
 </script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: center;
+  font-size: 60px;
+}
+
+.terminal {
+  display: flex;
+  justify-content: center;
+}
+</style>
 
 
